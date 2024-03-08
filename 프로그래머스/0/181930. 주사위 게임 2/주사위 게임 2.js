8facccde025f351a6b8f7de10bd3e783cdc64ac1
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-03-08 17:27:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+     switch(new Set([a,b,c]).size){
+        case 1 :
+        	return (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
+        case 2 :
+            return (a+b+c) * (a**2 + b**2 + c**2);
+        case 3 :
+            return a+b+c;
+    }
+}
