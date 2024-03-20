9de48e9e5fb8ef5ae9c11f0ad081a41e0853651c
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-03-20 15:44:06 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,17 @@
+def solution(my_string):
+    answer = 0
+    stack = []
+    for item in my_string:
+        if item.isdigit():
+            stack.append(item)
+        else:
+            if len(stack) != 0:
+                temp = int("".join(stack))
+                answer += temp
+                stack.clear()
+                
+    if len(stack) != 0:
+        temp = int("".join(stack))
+        answer += temp
+        stack.clear()
+    return answer
