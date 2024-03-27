3905e1ba8fccf3e95b443f779a616210e6e644af
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-03-27 17:24:10 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,13 +2,9 @@ word = input()
 
 
 def solution(value):
-    cnt = 0
     check = ["c=", "c-", "dz=", "lj", "d-", "nj", "s=", "z="]
     for item in check:
-        if item in value:
-            cnt += 1
-            value = value.replace(item, "A")
+            value = value.replace(item, "*")
     return len(value)
-
 
 print(solution(word))
