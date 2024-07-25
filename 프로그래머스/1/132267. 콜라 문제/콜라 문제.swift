08512224f43e31b5b465b0e8e2ba399eb0eb762f
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-07-25 15:34:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.17 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
+    // 받은 콜라
+    var cnt = 0
+    // 빈 병
+    var bottles = n
+    
+    while bottles >= a {
+        let tmp = Int(bottles / a) * b
+        cnt += tmp
+        bottles = bottles % a + tmp
+    } 
+    return cnt
+}
