b2e463a2fb81294e61bb0abb30280fe59e7a6074
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-10-11 22:23:42 +0900

    [level 2] Title: 전화번호 목록, Time: 477.25 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,10 @@
 def solution(phone_book):
-    
-    answer = True
     tb = set(phone_book)
     
     for item in tb:
         for idx in range(len(item)):
             prefix = item[:idx]
             if prefix in tb:
-                answer = False
-                break
+                return False
     
-    return answer
+    return True
