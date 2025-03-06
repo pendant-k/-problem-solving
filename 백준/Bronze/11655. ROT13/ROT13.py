81c46c93f4db66db8c53a9bf7e2314e870b26cc6
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-03-06 11:10:13 +0900

    [Bronze I] Title: ROT13, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,28 @@
+lowers = "abcdefghijklmnopqrstuvwxyz"
+uppers = lowers.upper()
+
+
+def rot13(c):
+    if c in lowers:
+        computed = ord(c) + 13
+        if computed > ord("z"):
+            return chr(ord('a') - 1 + computed - ord("z"))
+        else:
+            return chr(computed)
+
+    if c in uppers:
+        computed = ord(c) + 13
+        if computed > ord("Z"):
+            return chr(ord('A') - 1 + computed - ord("Z"))
+        else:
+            return chr(computed)
+
+    return c
+
+
+S = input()
+rst = ""
+for c in S:
+    rst += rot13(c)
+
+print(rst)
