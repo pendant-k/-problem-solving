2650644b2fe2357334b855ac3c3219cdb2b4915a
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-03-25 22:15:22 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8156 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+# 각 숫자의 갯수 저장
+ret = [0] * 10001
+for _ in range(n):
+    num = int(input())
+    ret[num] += 1
+
+for i in range(len(ret)):
+    if ret[i] != 0:
+        for j in range(ret[i]):
+            print(i)
