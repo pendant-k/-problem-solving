f83615564a381fece2790f1c5c2af04cc4d9cb4f
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-06-09 15:20:06 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.76 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,12 +1,13 @@
 function solution(num_list) {
     let ret = 0;
     
-    const odd = num_list.filter((num)=> num % 2 !== 0);
-    const oddConverted = odd.map((el)=> String(el)).join("");
+    const odd = num_list.filter((num)=> num % 2 !== 0).join("");
+    const even = num_list.filter((num)=> num % 2 === 0).join("");
     
-    const even = num_list.filter((num)=> num % 2 === 0);
-    const evenConverted = even.map((el)=> String(el)).join("");
+    // convert할 필요가 없음
     
-    
-    return Number(oddConverted) + Number(evenConverted);
+    // const oddConverted = odd.map((el)=> String(el)).join("");
+    // const evenConverted = even.map((el)=> String(el)).join("");
+    // return Number(oddConverted) + Number(evenConverted);
+    return Number(odd) + Number(even);
 }
