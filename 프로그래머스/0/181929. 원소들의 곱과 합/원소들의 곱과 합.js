05375e92e65e45e58930fe7666802507e3bf7cca
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-06-09 14:44:42 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,11 +1,5 @@
 function solution(num_list) {
-    var mul = 1;
-    var sum = 0;
-    
-    for(var i of num_list){
-        mul *= i;
-        sum += i;
-    }
-    
-    return +(mul < sum**2);
+    const tempA = num_list.reduce((a,b) => {return a + b},0);
+    const tempB = num_list.reduce((a,b) => {return a * b},1);
+    return tempB < tempA * tempA ? 1 : 0;
 }
