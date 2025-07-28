a9dfb1d975e63b0ad81b13c97a49f0ae9f1bc13a
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-07-29 00:30:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.87 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,24 +1,34 @@
 function solution(participant, completion) {
-    let answer = "";
-    
-    const hash = new Map();
-    
-    completion.forEach((e)=>{
-        if(!hash.has(e)){
-            hash.set(e,1);
+    let rst = '';
+    // js에서 객체 생성에는 hashmap 보다 성능이 좋은 방식을 활용함
+    const hash = {};
+    // 1. init hash
+    for(let i=0; i<participant.length;i++){
+        const currentKey = participant[i];
+        if(hash[currentKey]){
+            hash[currentKey]++;
         } else {
-            hash.set(e, hash.get(e) + 1);
-        }
-    });
-    
-    for(let v of participant){
-        if(!hash.has(v)){
-            return v;
-        }
-        if(hash.get(v) >= 1){
-            hash.set(v,hash.get(v) - 1);
-        } else {
-           return v;
+            hash[currentKey] = 1;
         }
     }
+    
+    // 2. loop 순회, 카운트 빼기
+    for(let i=0; i<completion.length; i++){
+        const currentKey = completion[i];
+        if(hash[currentKey] === 0) {
+            rst = currentKey;
+            break;
+        } else {
+            hash[currentKey]--;
+        }
+    }
+    // 3. 완주 못한 사람 확인
+    for(let key in hash){
+        if(hash[key] > 0){
+            rst = key;
+            break;
+        }
+    }
+    
+    return rst;
 }
