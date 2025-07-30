391f49869d4a631af012eee6ff18b937c035fc98
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-07-30 15:58:49 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,49 @@
+// validation -> 1 loop, indexing 2 word simultaneously
+function solution(begin, target, words) {
+    // constants
+    const wordLength = words[0].length;
+    // console.log(wordLength);
+    
+    // answer variable
+    let answer = 0;
+    
+    // target이 배열에 없다면 곧바로 종료 : O(n)
+    if(words.filter((el) => el === target).length === 0){
+        return 0;
+    }
+    
+    // bfs 수행
+    const visited = Array(words.length).fill(false);
+    // visited[0] = true;
+    // console.log(visited);
+    // queue data => [word, count]
+    const queue = [[begin,0]];
+    
+    while(queue.length > 0){
+        // queue 첫번째 데이터 조회
+        const [currentWord, distance] = queue.shift();
+        if(currentWord === target){
+            return distance;
+        }
+        // 다음 가능한 단어를 큐에 삽입
+        for(const [idx,word] of words.entries()){
+            // 일치하는 문자 카운트
+        	let cnt = 0
+            for(let i=0; i<wordLength; i++){
+                if(currentWord[i] === word[i]) cnt++;
+            }
+            
+            if(cnt === wordLength -1 && !visited[idx]){
+                visited[idx] = true;
+                queue.push([word, distance +1]);
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    
+    return answer;
+}
