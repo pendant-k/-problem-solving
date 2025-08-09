80cfffbb0ae8417169ea1fe121c0a70ac167dfd6
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-08-09 20:28:14 +0900

    [level 3] Title: 정수 삼각형, Time: 6.53 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,37 +1,32 @@
 function solution(triangle) {
     // 푸는 방향은 맞음
     const dp = [];
-    for(const node of triangle){
-        dp.push(Array(node.length).fill(0));
+    
+    for(const row of triangle){
+        dp.push(Array(row.length).fill(0));
     }
     
-    // console.log(triangle[3]);
-    // 모든 점 방문하면서 dp 배열 채우기
+    // 최초값 초기화
     dp[0][0] = triangle[0][0];
+    
     for(let y =1; y<triangle.length; y++){
         for(let x = 0; x < triangle[y].length; x++){
-            if(y === 1){
-                dp[y][x] = dp[0][0] + triangle[y][x];
-            } else {
-                // 오른쪽 끝
-                if(x === triangle[y].length -1){
-            		dp[y][x] = dp[y-1][x-1] + triangle[y][x];
-                } else if(x === 0) {
-            		dp[y][x] = dp[y-1][x] + triangle[y][x];
-                } else {
-            		dp[y][x] = Math.max(dp[y-1][x], dp[y-1][x-1]) + triangle[y][x];
-                }
+            
+            // 오른쪽 끝인 경우
+            if(x === y){
+                dp[y][x] = dp[y-1][x-1] + triangle[y][x];
+            } 
+            // 왼쪽 끝인 경우
+            else if(x === 0) {
+                dp[y][x] = dp[y-1][x] + triangle[y][x];
+            } 
+            // 가운데인 경우
+            else {
+                dp[y][x] = Math.max(dp[y-1][x], dp[y-1][x-1]) + triangle[y][x];
             }
-            // console.log(dp);
+            
         }
     }
-    // console.log(dp);
-    // 첫번째 값 초기화
-    // dp[0][0] = triangle[0][0];
-    // console.log(dp[0][0]);
-    // dp[y][x] = Math.max(dp[y-1][x-1], dp[y-1][x])
-    
-    // console.log(dp[dp.length-1]);
     
     const ret =  Math.max(...dp[dp.length-1]);
     return ret;
