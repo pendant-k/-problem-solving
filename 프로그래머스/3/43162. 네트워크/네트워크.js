9b7535f80444a8f4edec816d0c4d8f9888a4cff1
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-08-10 01:18:42 +0900

    [level 3] Title: 네트워크, Time: 6.21 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,26 +1,29 @@
 function solution(n, computers) {
-    let cnt = 0;
-    const visited = Array(n).fill(false);
+    // 기본값은 1 (최소)
+    var answer = 1;
     
-    const dfs = (node) => {
-        visited[node] = true;
+    let visited = Array(n).fill(false);
+    visited[0] = true;
+    
+    // 각 정점마다 연결된 모든 값 방문 처리
+    // 0부터 n
+    // 각 노드에 연결된 노드는 computers[i][j]임
+    for(let i =0; i < computers.length; i++){
+        if(!visited[i]) answer++;
         
-        for(let neighbor=0; neighbor<n; neighbor++){
-            if(computers[node][neighbor] === 1 && !visited[neighbor] ){
-                dfs(neighbor);
+    	let queue = [i];
+        while(queue.length > 0) {
+            const currentNode = queue.shift();
+            for(let neighbor = 0; neighbor < computers[currentNode].length; neighbor++){
+                if(neighbor === currentNode) continue;
+                // 연결된 경우
+                if(computers[currentNode][neighbor] === 1 && !visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
             }
         }
-        return;
     }
     
-    
-    for(let i =0; i < n; i++){
-        if(!visited[i]){
-            cnt++;
-        	dfs(i);
-        }
-    }
-    
-    return cnt;
+    return answer;
 }
-
