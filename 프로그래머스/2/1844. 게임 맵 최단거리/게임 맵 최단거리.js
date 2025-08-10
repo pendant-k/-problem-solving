e816390479fe2312cebfbd4623b8217667f39bde
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-08-10 12:16:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 32.07 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,43 +1,33 @@
-// 최단거리 => 아묻따 BFS
-// 효율성 탈락 -> Queue 맹글어쓰자...
+
 function solution(maps) {
-	// 상하좌우 이동할 때 씀   
-    const dy = [1,-1,0,0];
-    const dx = [0,0,1,-1];
+    const dy = [0,0,1,-1];
+    const dx = [1,-1,0,0];
     
-    const visited = Array.from({ length: maps.length }).map(
-        () => Array(maps[0].length).fill(false)
-    );
+    // y , x , dist
+    let queue = [[0,0,1]];
     
-    // console.log(visited);
-    
-    const bfs = () => {
-        // y,x,distance
-        const queue = [[0,0,1]];
-        while(queue.length > 0){
-            const [y,x,distance] = queue.shift()
+    while(queue.length > 0){
+        const [cy,cx, dist] = queue.shift()
+        
+        if(cy === maps.length-1 && cx === maps[0].length -1){
+            return dist;
+        }
+        
+        for(let i =0; i < 4; i++){
+            const ny = cy + dy[i];
+            const nx = cx + dx[i];
             
-            // 도착했는지 확인
-            if(y === maps.length -1 && x === maps[0].length -1) {
-                return distance;
-            } 
-            
-            for(let i = 0; i<4; i++){
-                // 다음 움직이는 좌표
-                const ny = y + dy[i];
-                const nx = x + dx[i];
-                
-                // 칸 유효성 체크
-                if(ny >=0 && nx >=0 && ny <= maps.length -1 && nx <= maps[0].length -1) {
-                    if(maps[ny][nx] === 1 && !visited[ny][nx]){
-                    	queue.push([ny,nx, distance +1]);
-            			visited[ny][nx] = true;
-                    }
+            // 유효한 좌표일 때
+            if(ny >=0 && ny < maps.length && nx >=0 && nx < maps[0].length){
+                // 방문하지 않은 경우에만
+                if(maps[ny][nx] === 1){
+                    queue.push([ny,nx, dist+1]);
+                    // 방문 처리
+                    maps[ny][nx]++;
                 }
             }
         }
-        return -1
     }
     
-    return bfs();
+    return -1
 }
